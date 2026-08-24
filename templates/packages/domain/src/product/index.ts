77abc6e7a62a29d5bf68fb01844ede114b6a919f
Lizardo Romero Scott <lrscott83@gmail.com>
2026-08-24 18:39:50 -0400

    chore(domain): export import-helpers from the product barrel

--- templates/packages/domain/src/product/index.ts
+++ templates/packages/domain/src/product/index.ts
@@ -4,3 +4,4 @@ export * from './pricing.js';
 export * from './product-repository.port.js';
 export * from './category-repository.port.js';
 export * from './errors.js';
+export * from './import-helpers.js';
